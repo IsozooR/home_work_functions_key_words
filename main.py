c9b71787922a2a037541bc8233bb0b3228e68786
IsozooR <artem.e04@mail.ru>
2025-12-23 06:43:19 +0300

    upd

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ def max_number(a, b):
     if a < b:
         return b
     else:
-        return "Values are same"
+        return a
+
 
 def empty_function():
     pass
+
 
 def even_numbers(n):
     values = [numbers for numbers in range (0, n+1) if numbers % 2 == 0]
     for numeral in values:
         yield numeral
 
+
 def test_max_number():
     assert max_number(0, 2) == 2, "Ошибка, Б должно быть больше, чем А"
     assert max_number(-3, -6) == -3, "Ошибка, А должно быть больше, чем Б "
-    assert max_number(0, 0) == "Values are same", "Ошибка, значения одинаковы"
+    assert max_number(0, 0) == 0, "Ошибка, значения одинаковы"
+    print("\nВсе тесты успешно пройдены")
 
 
 try:
@@ -29,4 +33,5 @@ except ValueError:
 for number in even_numbers(user_value): # пример работы функции even_numbers():
     print(number)
 
+test_max_number()
 
